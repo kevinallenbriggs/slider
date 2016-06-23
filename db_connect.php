69e49c6bd3ca293bada23e1d4ae17ac3fd8124b2
Kevin Briggs <redisforlosers@gmail.com>
2016-06-23 13:02:10 -0600

    updated directories and paths within the files

--- db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbhost		= 'localhost';
-$dbname		= 'teenseen';
-$dbuser		= 'teenseen';
-$dbpass		= 'teens';
-
-$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-if ($connection->connect_error) die($connection->connect_error);
-?>
