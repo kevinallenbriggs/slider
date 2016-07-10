44aad987f355d45d9716a451df9b0dc21a8e921a
Kevin Briggs <redisforlosers@gmail.com>
2016-07-10 11:46:39 -0600

    added app constants

--- settings.php
+++ settings.php
@@ -1,7 +1,26 @@
 <?php
 	/**
-	 * All global settings for this configuration of the applicaton can be set here
+	 * All global settings for this instance of the applicaton can be set here
 	 */
 	 
+
+	/**
+	 * 
+	 * @var APP_NAME (string) - This will display in the title of admin.php
+	 */
 	define("APP_NAME", 'TeenSeen');
+	
+	
+	/**
+	 * 
+	 * @var SLIDE_DURATION (integer) - The number of seconds each slide will display (in milliseconds)
+	 */
+	define("SLIDE_DURATION", 6000);
+	
+	
+	/**
+	 * 
+	 * @var SLIDE_TRANSITION (integer) - The number of seconds it takes to transition between slides (in milliseconds)
+	 */
+	define("SLIDE_TRANSITION", 1200);
 ?>
