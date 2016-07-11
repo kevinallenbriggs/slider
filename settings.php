dadaad793b9a7f6736a905b49121bef4022079da
Kevin Briggs <redisforlosers@gmail.com>
2016-07-10 22:49:13 -0600

    added SLIDE_AGE to system settings

--- settings.php
+++ settings.php
@@ -13,14 +13,21 @@
 	
 	/**
 	 * 
-	 * @var SLIDE_DURATION (integer) - The number of seconds each slide will display (in milliseconds)
+	 * @var SLIDE_DURATION (number) - The number of seconds each slide will display
 	 */
-	define("SLIDE_DURATION", 6000);
+	define("SLIDE_DURATION", 6);
 	
 	
 	/**
 	 * 
-	 * @var SLIDE_TRANSITION (integer) - The number of seconds it takes to transition between slides (in milliseconds)
+	 * @var SLIDE_TRANSITION (number) - The number of seconds it takes to transition between slides
 	 */
-	define("SLIDE_TRANSITION", 1200);
+	define("SLIDE_TRANSITION", 1.2);
+	
+	
+	/**
+	 * 
+	 * @var SLIDE_AGE (integer) - The number of days a slide will be kept after it has expired
+	 */
+	define("SLIDE_AGE", 60);
 ?>
