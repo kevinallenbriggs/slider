e82ffedf9383973f289e4c3052bf45707f0d3712
Kevin Briggs <redisforlosers@gmail.com>
2016-08-26 00:54:29 -0600

    completed removal of posts object

--- app/controllers/slides_controller.php
+++ app/controllers/slides_controller.php
@@ -1,13 +1,13 @@
 <?php
   class SlideController {
     public function index() {
-      // we store all the posts in a variable
+      // we store all the slides in a variable
       $slides = Slide::all();
       require_once('views/slides/index.php');
     }
 
     public function show() {
-      // we expect a url of form ?controller=posts&action=show&id=x
+      // we expect a url of form ?controller=slides&action=show&id=x
       // without an id we just redirect to the error page as we need the post id to find it in the database
       if (!isset($_GET['id']))
         return call('pages', 'error');
