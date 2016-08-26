b4536ceecabc5f82a4ba2d15d58d50a78bc6b235
Kevin Briggs <redisforlosers@gmail.com>
2016-08-25 22:19:39 -0600

    minor changes to file formats, no functional changes made

---
+++ app/views/layout.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+  <head>
+		<?php
+			// grab all the css files in the views folder
+			// this can be done better by bundling css files with MVC4,
+			// but that can be done on a future update
+	  		echo '<link rel="stylesheet" href="pages/error.css" />';
+	  	?>
+  </head>
+  <body>
+    <header>
+      <a href='/'>Home</a>
+      <a href='?controller=posts&action=index'>Posts</a>
+    </header>
+
+    <?php 
+    	$path = __DIR__ . "/../../routes.php";
+    	if(file_exists($path)) {
+    		require_once ($path);
+    	}?>
+
+    <footer>
+      Footer text
+    </footer>
+  <body>
+</html>
