4fd1b01328024a8602db5714e900da32a5062e14
Kevin Briggs <redisforlosers@gmail.com>
2016-08-26 22:43:41 -0600

    added pdo connection options and documentation

--- connection.php
+++ connection.php
@@ -8,13 +8,21 @@
 
     public static function getInstance() {
       if (!isset(self::$instance)) {
+      	
+      	// set the connection options which will apply to the entire app
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
         $pdo_options[PDO::ATTR_DEFAULT_FETCH_MODE] = PDO::FETCH_ASSOC;
+        $pdo_options[PDO::ATTR_EMULATE_PREPARES] = false;
+        
+        // database credentials
         $database	=	'slider';
         $username	=	'slider';
         $password	=	'nFTsX8GjQzYbKV6a';
+        
+        // create the new singleton instance (connection to database)
         self::$instance = new PDO("mysql:host=localhost;dbname=$database", $username, $password, $pdo_options);
       }
+      
       return self::$instance;
     }
   }
