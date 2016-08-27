8d76dabbf235c71dbeb66e37262fd2b6ebc39cf6
Kevin Briggs <redisforlosers@gmail.com>
2016-08-27 00:46:16 -0600

    commented view

--- app/views/slides/show.php
+++ app/views/slides/show.php
@@ -1,13 +1,18 @@
 <p>This is the requested slide:</p>
 
 <?php 
+	$path = '';
+	
+	// display the properties of the slide object
 	foreach($slide as $key => $value) {
 		echo "<p>$key: $value</p>";
 		
+		// remember the path so that it can be used to display the image
+		// after all of the Slide properties are done being read
 		if ($key == 'path_to_image' && !empty($value)) {
 			$path = $value;
 		}
 	}
 	
-	echo "<img src='$path' />";
+	echo "<img src='$path' />";		// display the slide
 ?>
