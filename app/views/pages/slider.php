d36664c9595ee683e95d872269cf2fbb20e89d31
Kevin Briggs <redisforlosers@gmail.com>
2016-09-09 19:11:27 -0600

    final commit before merging into master

--- app/views/pages/slider.php
+++ app/views/pages/slider.php
@@ -1,37 +1,38 @@
 <?php
 	class SliderView {
 		
+		// disable creation of SliderView objects
 		private function __construct() {}
 		private function __clone() {}
 		
+		// display the slider
 		public static function displaySlider($slides) {
 			$i = 1;
-			echo '<div class="slideshow-container">';
+			?><div class="slideshow-container"><?php
 			
 			foreach ($slides as $slide) {
-				echo '<div class="mySlides fade">';
-				echo "<div class='numbertext'>$i / 3</div>";
-				echo "<img src='$slide->path_to_image' style='width:100%'>";
-				echo "<div class='text'>$slide->caption</div>";
-				echo "</div>";
+				echo '<div class="mySlides fade">' .
+					 "<div class='numbertext'>$i / 3</div>" .
+					 "<img src='$slide->path_to_image' style='width:100%'>" .
+					 "<div class='text'>$slide->caption</div>" .
+					 "</div>";
 				$i++;
 			}
 			
-			echo <<<_EOT
+			?>
 
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-</div>
-<br>
+				<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+				<a class="next" onclick="plusSlides(1)">&#10095;</a>
+			</div>
+			<br>
 
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
-</div>
-_EOT;
+			<div style="text-align:center">
+  				<span class="dot" onclick="currentSlide(1)"></span> 
+  				<span class="dot" onclick="currentSlide(2)"></span> 
+  				<span class="dot" onclick="currentSlide(3)"></span> 
+			</div>
+			<?php
 		
-		}
+		}		// end of function displaySlider
 	}
-	
 ?>
