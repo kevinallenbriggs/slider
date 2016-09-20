a2c4c75156b4b718f7cd3d37d3eb7bdd1399f63b
Kevin Briggs <redisforlosers@gmail.com>
2016-09-20 17:42:20 -0600

    removed setting options until a later commit

--- app/views/pages/slider.php
+++ app/views/pages/slider.php
@@ -17,7 +17,6 @@
 				// TODO: add check for aspect ratio so that image can be displayed without scrolling
 
 				echo '<div class="mySlides fade">' .
-					 "<div class='numbertext'>$i / $numSlides</div>" .
 					 "<img src='$slide->path_to_image'>" .
 					 "<div class='text'>$slide->caption</div>" .
 					 "</div>";
@@ -26,16 +25,9 @@
 			
 			?>
 
-				<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-				<a class="next" onclick="plusSlides(1)">&#10095;</a>
 			</div>
 			<br>
 
-			<div style="text-align:center">
-  				<span class="dot" onclick="currentSlide(1)"></span> 
-  				<span class="dot" onclick="currentSlide(2)"></span> 
-  				<span class="dot" onclick="currentSlide(3)"></span> 
-			</div>
 			<script src="views/pages/slider.js"></script>
 
 			<?php
