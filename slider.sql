c0b47b3bf6ba100b9c15b952ea9b192b759baf2b
Kevin Briggs <redisforlosers@gmail.com>
2017-03-14 11:36:42 -0600

    update export attributes to include db connection and update SQL dump of DB

--- slider.sql
+++ slider.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 20, 2016 at 08:48 PM
+-- Generation Time: Mar 14, 2017 at 06:35 PM
 -- Server version: 5.6.33
 -- PHP Version: 5.6.27
 
@@ -17,10 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `slider`
+-- Database: `dev_slider`
 --
-CREATE DATABASE IF NOT EXISTS `slider` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `slider`;
 
 -- --------------------------------------------------------
 
@@ -28,16 +26,21 @@ USE `slider`;
 -- Table structure for table `settings`
 --
 
-DROP TABLE IF EXISTS `settings`;
-CREATE TABLE IF NOT EXISTS `settings` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `settings` (
+  `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(16) DEFAULT NULL,
   `value` varchar(32) DEFAULT NULL,
   `type` varchar(8) NOT NULL DEFAULT 'varchar',
   `tooltip` tinytext,
-  `image` varchar(255) NOT NULL DEFAULT 'gear.png',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `image` varchar(255) NOT NULL DEFAULT 'gear.png'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `name`, `value`, `type`, `tooltip`, `image`) VALUES
+(3, 'slide_duration', '10', 'varchar', NULL, 'gear.png');
 
 -- --------------------------------------------------------
 
@@ -45,19 +48,61 @@ CREATE TABLE IF NOT EXISTS `settings` (
 -- Table structure for table `slides`
 --
 
-DROP TABLE IF EXISTS `slides`;
-CREATE TABLE IF NOT EXISTS `slides` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `slides` (
+  `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(64) NOT NULL,
   `caption` varchar(255) DEFAULT NULL,
   `filename` varchar(255) NOT NULL,
   `type` varchar(24) DEFAULT NULL,
   `published` tinyint(1) DEFAULT NULL,
   `expires` date DEFAULT NULL,
-  `size` mediumint(8) UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+  `size` mediumint(8) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `slides`
+--
+
+INSERT INTO `slides` (`id`, `name`, `caption`, `filename`, `type`, `published`, `expires`, `size`) VALUES
+(4, 'FullSizeRender.jpg', 'test', 'fullsizerender.jpg', 'image/jpeg', 0, '0000-00-00', 165570),
+(5, 'moose', 'Just chillin\' at the resort in Steamboat Springs....', 'img_0356.jpg', 'image/jpeg', 0, '0000-00-00', 119607),
+(6, 'stick?', 'Got stick?', 'img_0483.jpg', 'image/jpeg', 0, '0000-00-00', 1417514),
+(7, 'Duncan\'s Ridge', 'About 25 feet up...', 'img_0486.jpg', 'image/jpeg', 1, '0000-00-00', 901497),
+(8, 'Duncan\'s Ridge 2', 'At the top about to belay down, about 45 feet up!', 'img_0488.jpg', 'image/jpeg', 1, '0000-00-00', 530794),
+(9, 'Duncan\'s Ridge 3', 'Taking a break before starting up the next pitch', 'img_0491.jpg', 'image/jpeg', 1, '0000-00-00', 721516),
+(11, 'Oranges', 'This route is called "Oranges" (the one right next to it is called "Apples") and it was right at the top of my ability level!', 'img_0936.jpg', 'image/jpeg', 1, '0000-00-00', 2223165),
+(12, 'Penny Dog', 'Penny looking interested in some weird thing only she could see or hear...', 'img_0970.jpeg', 'image/jpeg', 1, '0000-00-00', 1110732);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `slides`
+--
+ALTER TABLE `slides`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `slides`
+--
+ALTER TABLE `slides`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
